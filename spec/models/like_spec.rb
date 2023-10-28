a0ae8d4e1e9851d4909e5b05cd7defd9545f4a7e
Juan Diego Guerra <jdxelric@gmail.com>
2023-10-28 09:40:05 -0500

    Rubocop Linter Fixes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-    let(:user) do
-        User.create(name: 'JD', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Peru.')
-      end
-    
-      let(:post) do
-        Post.create(author: user, title: 'Hello', text: 'This is my first post')
-      end
-    
-      let(:like) do
-        Like.new(user:, post:)
-      end
-    
-      it 'should be valid with valid attributes' do
-        expect(like).to be_valid
-      end
-    
-      it 'belongs to the author' do
-        expect(like.user).to eq(user)
-      end
-    
-      it 'belongs to the post' do
-        expect(like.post).to eq(post)
-      end
+  let(:user) do
+    User.create(name: 'JD', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Peru.')
+  end
 
-      it 'updates post likes counter after save' do
-        expect { like.save }.to change { post.reload.likes_counter }.by(1)
+  let(:post) do
+    Post.create(author: user, title: 'Hello', text: 'This is my first post')
+  end
+
+  let(:like) do
+    Like.new(user:, post:)
+  end
+
+  it 'should be valid with valid attributes' do
+    expect(like).to be_valid
+  end
+
+  it 'belongs to the author' do
+    expect(like.user).to eq(user)
+  end
+
+  it 'belongs to the post' do
+    expect(like.post).to eq(post)
+  end
+
+  it 'updates post likes counter after save' do
+    expect { like.save }.to change { post.reload.likes_counter }.by(1)
   end
 end
