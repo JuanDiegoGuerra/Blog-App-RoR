87b0c1adb10a853b23dce707b8859c7a8307658e
Juan Diego Guerra <jdxelric@gmail.com>
2023-10-27 19:29:06 -0500

    Create Migrations

---
+++ db/migrate/20231028000111_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration[7.1]
+  def change
+    create_table :comments do |t|
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: { to_table: :posts }
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
