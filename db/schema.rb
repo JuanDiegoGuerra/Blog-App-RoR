f4c066c68ab09956c5d8e6a10fdb664c17332c23
Juan Diego Guerra <jdxelric@gmail.com>
2023-11-10 22:13:26 -0500

    Required Changes
    Styles+ DB Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_10_074824) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_10_053702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.bigint "author_id"
+    t.bigint "user_id"
     t.bigint "post_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_10_074824) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users", column: "author_id"
